c5b6363defe0c33e50e14fa86043174f33d70507
manoelbessa <manoel.bessa@gmail.com>
2026-02-10 13:55:59 -0300

    adicionado client de email

---
+++ src/main/java/com/javanauta/bffagendadortarefas/controller/EmailController.java
@@ -0,0 +1,25 @@
+package com.javanauta.bffagendadortarefas.controller;
+
+
+import com.javanauta.bffagendadortarefas.business.EmailService;
+import com.javanauta.bffagendadortarefas.business.dto.in.TarefasDTORequest;
+import com.javanauta.bffagendadortarefas.business.dto.out.TarefasDTOResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/email")
+public class EmailController {
+    private final EmailService emailService;
+
+    @PostMapping
+    public ResponseEntity<Void> enviarEmail(@RequestBody TarefasDTOResponse dto){
+        emailService.enviaEmail(dto);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/javanauta/bffagendadortarefas/infrastructure/client/EmailClient.java
@@ -0,0 +1,20 @@
+package com.javanauta.bffagendadortarefas.infrastructure.client;
+
+
+import com.javanauta.bffagendadortarefas.business.dto.in.EnderecoDTORequest;
+import com.javanauta.bffagendadortarefas.business.dto.in.LoginRequestDTO;
+import com.javanauta.bffagendadortarefas.business.dto.in.TelefoneDTORequest;
+import com.javanauta.bffagendadortarefas.business.dto.in.UsuarioDTORequest;
+import com.javanauta.bffagendadortarefas.business.dto.out.EnderecoDTOResponse;
+import com.javanauta.bffagendadortarefas.business.dto.out.TarefasDTOResponse;
+import com.javanauta.bffagendadortarefas.business.dto.out.TelefoneDTOResponse;
+import com.javanauta.bffagendadortarefas.business.dto.out.UsuarioDTOResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(name = "notificacao", url = "${notificacao.url}")
+public interface EmailClient {
+
+    void enviarEmail(@RequestBody TarefasDTOResponse dto);
+}
